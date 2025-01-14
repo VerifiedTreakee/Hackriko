b536571474608510f12bfd750d79bdbbd8431a17
VerifiedTreakee <114489533+VerifiedTreakee@users.noreply.github.com>
2025-01-15 00:05:32 +0530

    Update HaCricko.py

--- HaCricko.py
+++ HaCricko.py
@@ -1,130 +1,47 @@
 import random
+import sys
+from Funtions import P_toss
+from Funtions import C_toss
+while True:
+    print("Hacricko version idk")
+    print("---Menu---")
+    print("1. To start new game")
+    print("2. See previous Scores")
+    print("3. Exit")
 
-def P_toss():
-    print("Initializing toss")
-    print("Type '1' for Even Or '2' for Odd")
+    try:
+        menu = int(input("Enter a choice: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        continue
 
-    while True:
-        try:
-            pl = int(input("Odd ya Even?\n"))
-            if pl not in [1, 2]:
-                raise ValueError
-            break
-        except ValueError:
-            print("Invalid input. Please enter '1' for Even or '2' for Odd.")
-
-    if pl == 1:
-        print("Player chooses Even")
-    else:
-        print("Player chooses Odd")
-
-
-    while True:
-        try:
-            pl_toss = int(input("Enter choice (1-6): "))
-            if pl_toss < 1 or pl_toss > 6:
-                raise ValueError
-            break
-        except ValueError:
-            print("Invalid input. Please enter a number between 1 and 6.")
-
-    print("Player:", pl_toss)
-    com_toss = random.randint(1, 6)
-    print("Computer:", com_toss)
-
-    if (pl_toss + com_toss) % 2 == 0:
-        t = 1
-    else:
-        t = 2
-
-    if t == pl:
-        print("Player won the toss")
+    if menu == 1:
         while True:
             try:
-                c = int(input("Type '1' for Batting or '2' for Bowling: "))
-                if c not in [1, 2]:
+                P = int(input("Press 1 if you want to start or 2 if you want computer to start\n"))
+
+                if P not in [1,2]:
                     raise ValueError
-                break
+                else:
+                    break
             except ValueError:
-                print("Invalid input. Please enter '1' for Batting or '2' for Bowling.")
+                print("Invalid Input Please Enter 1 for you to start or 2 for computer to start!")
 
-        if c == 1:
-            p_choice = 'Batting'
-            c_choice = 'Bowling'
+        if P == 1:
+            p_choice,c_choice = P_toss()
+
         else:
-            p_choice = 'Bowling'
-            c_choice = 'Batting'
+            p_choice,c_choice = C_toss()
 
-        print(f"Player won the toss and chooses: {p_choice}")       
+   
+    elif menu == 2:
+        print("Feature not implemented yet.")
+    elif menu == 3:
+        sys.exit("Exiting the program.")
     else:
-        cc = ["Batting", "Bowling"]
-        c_choice = random.choice(cc)
-        if c_choice == "Batting":
-            p_choice = "Bowling"
+        print("Invalid input. Would you like to re-run the game? (y/n)")
+        n = input("Enter Choice: ").strip().lower()
+        if n[0] == 'y':
+            continue
         else:
-            p_choice = "Batting"
-        print(f"Computer won the toss and chooses: {c_choice}")
-
-
-    return p_choice,c_choice
-
-def C_toss():
-
-    print("Computer starts the game")
-    x = random.randint(1,2)
-
-    if x == 1:
-        
-        print("Computer choose Even")
-        pl = 2
-    else:
-        print("Computer choose Odd")
-        pl = 1
-
-    
-    com_toss = random.randint(1, 6)
-    print("Computer:", com_toss)
-    
-    while True:
-        try:
-            pl_toss = int(input("Enter choice (1-6): "))
-            if pl_toss < 1 or pl_toss > 6:
-                raise ValueError
-            break
-        except ValueError:
-            print("Invalid input. Please enter a number between 1 and 6.")
-
-    if (pl_toss + com_toss) % 2 == 0:
-        t = 1
-    else:
-        t = 2
-
-    if t == pl:
-        print("Player won the toss")
-        while True:
-            try:
-                c = int(input("Type '1' for Batting or '2' for Bowling: "))
-                if c not in [1, 2]:
-                    raise ValueError
-                break
-            except ValueError:
-                print("Invalid input. Please enter '1' for Batting or '2' for Bowling.")
-
-        if c == 1:
-            p_choice = 'Batting'
-            c_choice = 'Bowling'
-        else:
-            p_choice = 'Bowling'
-            c_choice = 'Batting'
-
-        print(f"Player won the toss and chooses: {p_choice}")       
-    else:
-        cc = ["Batting", "Bowling"]
-        c_choice = random.choice(cc)
-        if c_choice == "Batting":
-            p_choice = "Bowling"
-        else:
-            p_choice = "Batting"
-        print(f"Computer won the toss and chooses: {c_choice}")
-
-    return p_choice,c_choice
+            sys.exit("Invalid Input")
